57f73cab7283de69bba097a37554f4cfec3eebe3
Doug Anson <doug.anson@arm.com>
2016-11-30 10:52:50 +0800

    updated security.h

--- security.h
+++ security.h
@@ -13,43 +13,24 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+#error "Please update this security.h with your own: https://connector.mbed.com/#credentials"
+
 #ifndef __SECURITY_H__
 #define __SECURITY_H__
  
 #include <inttypes.h>
  
-#define MBED_DOMAIN "12345678-3a32-3e5e-50d0-7a7668b90e6e"
-#define MBED_ENDPOINT_NAME "ded911c5-c24f-43cf-8365-8d23bb01c707"
+#define MBED_DOMAIN "12345678"
+#define MBED_ENDPOINT_NAME "12345678"
  
 const uint8_t SERVER_CERT[] = "-----BEGIN CERTIFICATE-----\r\n"
-"MIIBmDCCAT6gAwIBAgIEVUCA0jAKBggqhkjOPQQDAjBLMQswCQYDVQQGEwJGSTEN\r\n"
-"MAsGA1UEBwwET3VsdTEMMAoGA1UECgwDQVJNMQwwCgYDVQQLDANJb1QxETAPBgNV\r\n"
-"DKDIEJDITSBtYmVkMB4XDTE1MDQyOTA2NTc0OFoXDTE4MDQyOTA2NTc0OFowSzEL\r\n"
-"MAkGA1UEBhMCRkkxDTALBgNVBAcMBE91bHUxDDAKBgNVBAoMA0FSTTEMMAoGA1UE\r\n"
-"CwwDSW9UMREwDwYDVQQDDAhBUk0gbWJlZDBZMBMGByqGSM49AgEGCCqGSM49AwEH\r\n"
-"A0IABLuAyLSk0mA3awgFR5mw2RHth47tRUO44q/RdzFZnLsAsd18Esxd5LCpcT9w\r\n"
-"0tvNfBv4xJxGw0wcYrPDDb8/rjujEDAOMAwGA1UdEwQFMAMBAf8wCgYIKoZIzj0E\r\n"
-"AwIDSAAwRQIhAPAonEAkwixlJiyYRQQWpXtkMZax+VlEiS201BG0PpAzAiBh2RsD\r\n"
-"NxLKWwf4O7D6JasGBYf9+ZLwl0iaRjTjytO+Kw==\r\n"
 "-----END CERTIFICATE-----\r\n";
  
 const uint8_t CERT[] = "-----BEGIN CERTIFICATE-----\r\n"
-"MIIBzzCCAXOgAwIBAgIEVvwvcjAMBggqhkjOPQQDAgUAMDkxCzAJBgNVBAYTAkZ\r\n"
-"JMQwwCgYDVQQKDANBUk0xHDAaBgNVBAMME21iZWQtY29ubmVjdG9yLTIwMTYwHh\r\n"
-"cNMTYwMjE3MTYyNDE3WhcNMTYxMjMxMDYwMDAwWjCBoTFSMFADJKDEJxNJZGVjZ\r\n"
-"DA2Y2MtMmEzMi00ZTVlLTgwZDAtN2E3YzY1YjkwZTZlL2NjNjllN2M1LWMyNGYt\r\n"
-"NDdkdo2jKzY1LThkMjNiYjAxYzcwNzEMMAoGA1UECxMDQVJNMRIwEAYDVQQKEwl\r\n"
-"tYmVkIHVzZXIxDTALBgNVBAcTBE91bHUxDTALBgNVBAgTBE91bHUxCzAJBgNVBA\r\n"
-"YTAkZJMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEkLLpLh0q/dljt2psfajmd\r\n"
-"riE2yW8InSNyJwe/Z4oh0BdaVxYlmdnQBCp0gLnchTWTTy6MB27hef8rMEvGmOK\r\n"
-"lTAMBggqhkjOPQQDAgUAA0gAMEUCICAEkEMRydAYgW8t6Lo4qVobF047d97VmQ4\r\n"
-"OtgsV9nlHAiEA/to6FFugWcIUZXL6mhn4Pm09ZYDeLXv7wCgXax2y8a8=\r\n"
 "-----END CERTIFICATE-----\r\n";
  
 const uint8_t KEY[] = "-----BEGIN PRIVATE KEY-----\r\n"
-"MIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgQ2Sur28lymqjNtkw\r\n"
-"49CXNmMXfglbzBMQFQdqAoGpOX+DLEOF(EWHGSM49Awd92PHJLLdkj2jdstqgbwi\r\n"
-"dI3InB79niiHQF1pXFiWZ2dAEKnSAudyFNZNPLowHbuF5/yswS8aY4qV\r\n"
 "-----END PRIVATE KEY-----\r\n";
  
 #endif //__SECURITY_H__
